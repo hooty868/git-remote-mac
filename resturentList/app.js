fe1f8191b9cb1b4f2ff00a13ce41f2c368e8eb68
hooty868 <hooty868@gmail.com>
2020-09-29 18:44:02 +0800

    inport express project
    1.install plugin: npm exprss express_handlebars bootstrap
    2.establish express.js enviroment
    3.define layout of main page

---
+++ resturentList/app.js
@@ -0,0 +1,23 @@
+// Include express from node_modules and define server related variables
+const express = require('express')
+const app = express()
+const port = 3000
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// setting the route and corresponding response
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// Listen the server when it started
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
