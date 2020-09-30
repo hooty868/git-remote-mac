38c0728bd7e26800d9c9a5f9a72a6e688102f0d7
hooty868 <hooty868@gmail.com>
2020-09-30 23:55:01 +0800

    establish the popular restaurant List

--- resturentList/app.js
+++ resturentList/app.js
@@ -12,9 +12,25 @@ app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
 
+// require restaurant list JASON file
+const restaurantList = require('./restaurant.json')
+
 // setting the route and corresponding response
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurant = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurant })
 })
 
 // Listen the server when it started
