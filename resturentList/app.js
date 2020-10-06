a19653725a8c5f7a77fe36e8fff99f1726a6111a
hooty868 <hooty868@gmail.com>
2020-10-06 13:46:56 +0800

    feat: add delete restaurant function

--- resturentList/app.js
+++ resturentList/app.js
@@ -102,6 +102,16 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
+
+// delete restaurant post
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return restaurantList.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // Listen the server when it started
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
