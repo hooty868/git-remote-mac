558a413fbb04194e5dbe376f02b26827d6e2e104
hooty868 <hooty868@gmail.com>
2020-10-18 12:03:50 +0800

    feat:add welcome user back label on search bar  in index.hbs

--- resturentList/routes/modules/restaurents.js
+++ resturentList/routes/modules/restaurents.js
@@ -132,13 +132,15 @@ const users = [
 router.post('/welcomeuser', (req, res) => {
   const userEmail = req.body.email
   const userPassword = req.body.Password
-  if ((users.some(e => (e.email === userEmail) && (e.password === userPassword)))) {
+  const userindex = (users.find(e => (e.email === userEmail) && (e.password === userPassword)))
+  if (userindex) {
+    const openinfo = { info: `歡迎${userindex.firstName}回來` }
     return restaurantList.find() // 取出 Todo model 裡的所有資料
       .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-      .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+      .then(restaurants => res.render('index', { restaurants, openinfo })) // 將資料傳給 index 樣板
       .catch(error => console.error(error)) // 錯誤處理
   } else {
-    const errorinfo = { error: '..... email or password is error' }
+    const errorinfo = { error: '..... Username 或Password 錯誤' }
     return restaurantList.find() // 取出 Todo model 裡的所有資料
       .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
       .then(restaurants => res.render('login', { restaurants, errorinfo })) // 將資料傳給 index 樣板
